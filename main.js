3e30847e86993de0a8e7665f67f788321c4a3571
Afzal Ismail <afzalismail96@gmail.com>
2026-05-12 16:01:17 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -1,12 +1,58 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let mainWindow;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    show: false, // prevents white flash
   });
 
-  win.loadFile('public/index.html');
+  mainWindow.loadFile('public/index.html');
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-app.whenReady().then(createWindow);
+function setupAutoUpdater() {
+  // optional delay avoids startup slowdown
+  setTimeout(() => {
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 3000);
+
+  autoUpdater.on('update-available', () => {
+    console.log('Update available');
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    console.log('Update downloaded');
+    // optional auto-install:
+    // autoUpdater.quitAndInstall();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  setupAutoUpdater();
+
+  // macOS best practice
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed (except macOS)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
